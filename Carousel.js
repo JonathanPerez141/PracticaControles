7984b631a9d25ddd2ed833dc69662a03090de67d
Jonathan Perez <a1120150052@utch.edu.mx>
2023-06-02 17:25:48 -0700

    Video del putin

---
+++ Carousel.js
@@ -0,0 +1,43 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+let slideBox = 0;
+showSlide();
+
+function showSlide() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideBox++;
+  if (slideBox > slides.length) {slideBox = 1}
+  slides[slideBox-1].style.display = "block";
+  setTimeout(showSlide, 3000); // Change image every 2 seconds
+}
